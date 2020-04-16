1fe2daa23e0523275225a8035ad9f39477a5e510
andy darr <Andy@macbook-air.wowway.com>
2020-04-16 18:46:59 -0400

    mvp site content

--- public/src/App.js
+++ public/src/App.js
@@ -1,26 +1,73 @@
 import React from 'react';
-import logo from './images/moto.jpg';
-import './App.css';
+import { makeStyles} from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Header from './Header';
+import {GitHub, LinkedIn, Instagram} from '@material-ui/icons'
+import { Container, Typography, Avatar, Link, Box, Grid } from '@material-ui/core';
+import me from './images/me.jpeg'
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  mainGrid: {
+    marginTop: theme.spacing(3),
+  },
+  margin: {
+    marginBottom: theme.spacing(1),
+  },
+  avatar: {
+    width: '200px',
+    height: '200px'
+  }
+}));
+
+const links = [
+  {
+    icon: LinkedIn,
+    title: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/andrew-darr/',
+    text: "See where I've worked",
+  },
+  {
+    icon: Instagram,
+    title: 'Instagram',
+    url: 'https://www.instagram.com/andydarrr/',
+    text: "See my photos"
+  },
+  {
+    icon: GitHub,
+    title: 'Github',
+    url: 'https://github.com/c4ndybar',
+    text: "See my code"
+  },
+];
+
+export default function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi there, welcome to Andy's site!
-        </p>
-        <a
-          className="App-link"
-          href="https://www.instagram.com/andydarrr/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Check out my photos
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="md">
+        <Header sections={links} />
+        <main>
+          <Grid container spacing={5} className={classes.mainGrid}>
+            <Grid item xs={12} md={6}>
+              <Typography variant="h4">Hey there, I'm Andy Darr</Typography>
+              <Typography variant="body1">I build apps, travel, game, play music, and drink lots of coffee.</Typography>
+              <Box component="div" mt={2}>
+                <Typography variant="body1">Drop me a line</Typography>
+                  <div>
+                    <Link variant="body2" href="mailto:andrew.darr86@gmail.com">andrew.darr86@gmail.com</Link>
+                  </div>
+              </Box>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Box display="flex" justifyContent="center">
+                <Avatar src={me} className={classes.avatar} />
+              </Box>
+            </Grid>
+          </Grid>
+        </main>
+      </Container>
+    </React.Fragment>
   );
 }
-
-export default App;
---
+++ public/src/Header.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Link from '@material-ui/core/Link';
+
+const useStyles = makeStyles((theme) => ({
+  toolbar: {
+    overflowX: 'auto',
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+  toolbarLink: {
+    flexShrink: 0,
+    marginRight: theme.spacing(1),
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  inline: {
+    display: 'inline-flex',
+  }
+}));
+
+export default function Header(props) {
+  const classes = useStyles();
+  const { sections } = props;
+
+  return (
+    <React.Fragment>
+
+      <Toolbar component="nav" variant="dense" className={classes.toolbar}>
+        {sections.map((section) => {
+          const LinkIcon = section.icon;
+          return (
+            <Link
+              color="inherit"
+              noWrap
+              key={section.title}
+              variant="body2"
+              href={section.url}
+              className={classes.toolbarLink}
+            >
+              <LinkIcon />
+            </Link>
+          )
+        }
+        )}
+      </Toolbar>
+    </React.Fragment>
+  );
+}
+
+Header.propTypes = {
+  sections: PropTypes.array,
+};

eb4b2e5a7afbd1fc285abace7a651f9b9b6d5e60
andy darr <Andy@macbook-air.wowway.com>
2020-04-17 11:59:58 -0400

    adding xbl link and peace sign

--- public/src/App.js
+++ public/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { makeStyles} from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Header from './Header';
-import {GitHub, LinkedIn, Instagram} from '@material-ui/icons'
+import {GitHub as GitHub, LinkedIn, Instagram, SportsEsports} from '@material-ui/icons'
 import { Container, Typography, Avatar, Link, Box, Grid } from '@material-ui/core';
 import me from './images/me.jpeg'
 
@@ -38,6 +38,12 @@ const links = [
     url: 'https://github.com/c4ndybar',
     text: "See my code"
   },
+  {
+    icon: SportsEsports,
+    title: 'Xbox Live',
+    url: 'https://xboxgamertag.com/search/L337-Jesu5',
+    text: 'Game with me',
+  }
 ];
 
 export default function App() {
@@ -54,7 +60,7 @@ export default function App() {
               <Typography variant="h4">Hey there, I'm Andy Darr</Typography>
               <Typography variant="body1">I build apps, travel, game, play music, and drink lots of coffee.</Typography>
               <Box component="div" mt={2}>
-                <Typography variant="body1">Drop me a line</Typography>
+                <Typography variant="body1">Drop me a line ✌️</Typography>
                   <div>
                     <Link variant="body2" href="mailto:andrew.darr86@gmail.com">andrew.darr86@gmail.com</Link>
                   </div>
